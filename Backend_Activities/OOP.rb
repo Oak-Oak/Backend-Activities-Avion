3dad47b5d662fa9953423d4e1d00563899278217
GARS <christian.garcia4297@gmail.com>
2024-01-26 21:45:33 +0800

    Added Solutions and Comments for Abstraction and Encapsulation

--- Backend_Activities/OOP.rb
+++ Backend_Activities/OOP.rb
@@ -1,6 +1,6 @@
 #Encapsulation, Abstraction, Polymorphism using inheritance, Duck Typing
 
-#Encapsulation
+#For Encapsulation
 class Numbers
   def initialize( number1,number2)
     @number1 = number1
@@ -12,10 +12,10 @@ class Numbers
     puts computation
   end
 
-  private
+  private 
 
   def computation
-    @number1**2 + @number2**2
+    @number1**2 + @number2**2 #number1 and number2, are being encapsulated under computation method
   end
 end
 
@@ -25,6 +25,23 @@ numbers.display_info
 
 #Abstraction
 
+class Numbers
+  def initialize( number1,number2)
+    @number1 = number1
+    @number2 = number2
+  end
+
+  def display_info
+    puts "Give me 2 numbers and I'll give you the sum of its squares"
+    puts computation
+  end
+
+  private #abstracts/ hide how the computation is made
+
+  def computation
+    @number1**2 + @number2**2
+  end
+end
 
 
 
@@ -51,38 +68,37 @@ numbers.display_info
 
 
 
+#duck typing
+class Square
+  attr_accessor :side_length
 
-# #duck typing
-# class Square
-#   attr_accessor :side_length
+  def initialize(side_length)
+    @side_length = side_length
+  end
 
-#   def initialize(side_length)
-#     @side_length = side_length
-#   end
+  def area
+    side_length * side_length
+  end
+end
 
-#   def area
-#     side_length * side_length
-#   end
-# end
+class Circle
+  attr_accessor :radius
 
-# class Circle
-#   attr_accessor :radius
+  def initialize(radius)
+    @radius = radius
+  end
 
-#   def initialize(radius)
-#     @radius = radius
-#   end
+  def area
+    Math::PI * radius * radius
+  end
+end
 
-#   def area
-#     Math::PI * radius * radius
-#   end
-# end
+def calculate_area(shape) #doesn't check the type of shape, intstead it responds to area method
+  shape.area
+end
 
-# def calculate_area(shape)
-#   shape.area
-# end
+square = Square.new(5)
+circle = Circle.new(3)
 
-# square = Square.new(5)
-# circle = Circle.new(3)
-
-# puts "Area of the square: #{calculate_area(square)}"  #area of square
-# puts "Area of the circle: #{calculate_area(circle)}"   #area of circle
+puts "Area of the square: #{calculate_area(square)}"  #area of square
+puts "Area of the circle: #{calculate_area(circle)}"   #area of circle
